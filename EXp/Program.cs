22d720467fa462ffc9ff434473ad9af455e35e14
erdem <erdemahmet750@gmail.com>
2021-07-25 00:50:20 +0300

    Proje dosyası ekle.

---
+++ EXp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 5, 4, 7, 3, 8, 1 };
+            for (int i = numbers.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        var temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(",", numbers));
+            Console.ReadLine();
+        }
+    }
+}
